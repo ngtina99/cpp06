b5e67ac0c2876c5a4c926aba4312b45e982d0f2d
Tina <nguyen.valentina.99@gmail.com>
2025-02-02 23:35:33 +0100

    added

--- ex02/includes/Base.hpp
+++ ex02/includes/Base.hpp
@@ -6,12 +6,20 @@
 /*   By: ngtina1999 <ngtina1999@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/30 01:22:09 by ngtina1999        #+#    #+#             */
-/*   Updated: 2025/01/30 01:24:07 by ngtina1999       ###   ########.fr       */
+/*   Updated: 2025/02/02 22:54:47 by ngtina1999       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BASE_HPP
 #define BASE_HPP
+
+#include <iostream>
+
+#define MYEOF "\033[0m"
+#define MYRED "\033[1;31m"
+#define MYBLUE "\033[1;34m"
+#define MYGREEN "\033[1;32m"
+#define MYPURPLE "\033[1;35m"
 
 class Base {
 
--- ex02/sources/main.cpp
+++ ex02/sources/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ngtina1999 <ngtina1999@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/30 01:32:34 by ngtina1999        #+#    #+#             */
-/*   Updated: 2025/01/30 01:38:19 by ngtina1999       ###   ########.fr       */
+/*   Updated: 2025/02/02 23:35:21 by ngtina1999       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,86 @@
 #include "../includes/C.hpp"
 #include "../includes/Base.hpp"
 
-int	main() {
 
-Base * generate(void);
+Base *generate(void) {
+	switch(rand() % 3) {
+		case 0:
+			std::cout << MYBLUE << "A generated" << MYEOF << std::endl;
+			return(new A());
+		case 1:
+				std::cout << MYBLUE << "B generated" << MYEOF << std::endl;
+			return(new B());
+		case 2:
+			std::cout << MYBLUE << "C generated" << MYEOF << std::endl;
+			return(new C());
+		default:
+			std::cout << MYRED << "Error in generation" << MYEOF << std::endl;
+			return(NULL);
+	}
+}
 // It randomly instanciates A, B or C and returns the instance as a Base pointer. Feel free
 // to use anything you like for the random choice implementation.
 
-void identify(Base* p);
+void identify(Base* p) {
+	if (dynamic_cast<A*>(p) != NULL)
+		std::cout << "A is identified with a pointer" << std::endl;
+	else if (dynamic_cast<B*>(p) != NULL)
+		std::cout << "B is identified with a pointer" << std::endl;
+	else if (dynamic_cast<C*>(p) != NULL)
+		std::cout << "C is identified with a pointer" << std::endl;
+	else
+		std::cout << MYRED << "No type is identified" << MYEOF << std::endl;	
+}
 // It prints the actual type of the object pointed to by p: "A", "B" or "C".
 
-void identify(Base& p);
+void identify(Base& p) {
+	try {
+		A referenceA = dynamic_cast<A &>(p);
+		std::cout << "A is identified with a pointer" << std::endl;
+		return ;
+	}
+	catch (std::exception &e) {
+		std::cout << "Caught exception: " << e.what() << std::endl;
+	}
+	try {
+		B referenceB = dynamic_cast<B &>(p);
+		std::cout << "B is identified with a pointer" << std::endl;
+		return ;
+	}
+	catch (std::exception &e) {
+		std::cout << "Caught exception: " << e.what() << std::endl;
+	}
+	try {
+		C referenceC = dynamic_cast<C &>(p);
+		std::cout << "C is identified with a pointer" << std::endl;
+		return ;
+	}
+	catch (std::exception &e) {
+		std::cout << "Caught exception: " << e.what() << std::endl;
+	}
+}
+
 // It prints the actual type of the object pointed to by p: "A", "B" or "C". Using a pointer
 // inside this function is forbidden.
 
 
+int main() {
+    Base* obj = generate();
+    std::cout << "Identification by pointer: ";
+    identify(obj);
+    
+    std::cout << "Identification by reference: ";
+    identify(*obj);
+    
+    // Proof of downcasting
+    if (A* a = dynamic_cast<A*>(obj)) {
+        std::cout << "Downcasting succeeded: Object is of type A" << std::endl;
+    } else if (B* b = dynamic_cast<B*>(obj)) {
+        std::cout << "Downcasting succeeded: Object is of type B" << std::endl;
+    } else if (C* c = dynamic_cast<C*>(obj)) {
+        std::cout << "Downcasting succeeded: Object is of type C" << std::endl;
+    }
+    
+    delete obj;
+    return 0;
 }
