7cea44bcdf5f43a9830dd59096014dc1f989872b
Tina <nguyen.valentina.99@gmail.com>
2025-02-14 15:50:16 +0100

    added

--- ex02/sources/main.cpp
+++ ex02/sources/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ngtina1999 <ngtina1999@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/30 01:32:34 by ngtina1999        #+#    #+#             */
-/*   Updated: 2025/02/03 00:26:37 by ngtina1999       ###   ########.fr       */
+/*   Updated: 2025/02/14 15:50:04 by ngtina1999       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,13 @@
 #include "../includes/C.hpp"
 #include "../includes/Base.hpp"
 
-
 Base *generate(void) {
 	switch(rand() % 3) {
 		case 0:
 			std::cout << MYBLUE << "A generated" << MYEOF << std::endl;
 			return(new A());
 		case 1:
-				std::cout << MYBLUE << "B generated" << MYEOF << std::endl;
+			std::cout << MYBLUE << "B generated" << MYEOF << std::endl;
 			return(new B());
 		case 2:
 			std::cout << MYBLUE << "C generated" << MYEOF << std::endl;
@@ -35,7 +34,7 @@ Base *generate(void) {
 // It randomly instanciates A, B or C and returns the instance as a Base pointer. Feel free
 // to use anything you like for the random choice implementation.
 
-void identify(Base* p) {
+void	identify(Base* p) {
 	if (dynamic_cast<A*>(p) != NULL)
 		std::cout << "A is identified with a pointer" << std::endl;
 	else if (dynamic_cast<B*>(p) != NULL)
@@ -45,12 +44,11 @@ void identify(Base* p) {
 	else
 		std::cout << MYRED << "No type is identified" << MYEOF << std::endl;	
 }
-// It prints the actual type of the object pointed to by p: "A", "B" or "C".
 
-void identify(Base& p) {
+void	identify(Base& p) {
 	try {
 		A referenceA = dynamic_cast<A &>(p);
-		std::cout << "A is identified with a pointer" << std::endl;
+		std::cout << "A is identified with a reference" << std::endl;
 		return ;
 	}
 	catch (std::exception &e) {
@@ -69,36 +67,29 @@ void identify(Base& p) {
 	}
 	catch (std::exception &e) {
 	}
-    std::cout << MYRED << "No type is reference" << MYEOF << std::endl;
+	std::cout << MYRED << "No type is reference" << MYEOF << std::endl;
 }
 
-// It prints the actual type of the object pointed to by p: "A", "B" or "C". Using a pointer
-// inside this function is forbidden.
+int	main() {
 
+	srand(clock() - rand());
 
-int main() {
     Base* obj = generate();
     std::cout << "Identification by pointer: ";
     identify(obj);
     
     std::cout << "Identification by reference: ";
     identify(*obj);
-    
-	
-	
+
 	// Proof of downcasting
 	if (A* a = dynamic_cast<A*>(obj)) {
-		std::cout << "Downcasting succeeded: Object is of type A" << std::endl;
-		a->printType();  // Output specific to A
+		std::cout << MYGREEN "Downcasting succeeded: Object is of type A" MYEOF<< std::endl;
 	} else if (B* b = dynamic_cast<B*>(obj)) {
-		std::cout << "Downcasting succeeded: Object is of type B" << std::endl;
-		b->printType();  // Output specific to B
+		std::cout << MYGREEN "Downcasting succeeded" MYEOF<< ":" << MYBLUE " Object is of type B" MYEOF<< std::endl;
 	} else if (C* c = dynamic_cast<C*>(obj)) {
-		std::cout << "Downcasting succeeded: Object is of type C" << std::endl;
-		c->printType();  // Output specific to C
+		std::cout << MYGREEN "Downcasting succeeded: Object is of type C" MYEOF << std::endl;
 	} else {
-		std::cout << "Downcasting failed: Object is not of type A, B, or C" << std::endl;
+		std::cout << MYRED "Errord: downcasting failed" MYEOF<< std::endl;
 	}
     delete obj;
-    return 0;
 }
